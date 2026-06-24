36e50cdaf085121847960f00f1e65cc97b72ab5d
BioVerse Admin <bioverse@example.com>
2026-06-24 11:12:50 +0530

    Initial commit for BioVerse

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/BIOVERSE/',
   server: {
     port: 3000,
     open: true
   },
   build: {
+    target: 'esnext',
     outDir: 'dist',
     sourcemap: true
   }
